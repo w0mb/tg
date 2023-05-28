2b36368a90a55ce02254a05b26ac2215a88dd08c
w0mb <danil-kovalev-04@mail.ru>
2023-05-29 01:20:21 +0700

    чето наворотил, пока не работает(траблы с 32 mingw)

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main()
+int main(void)
 {
-    printf("helllo");
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Моя игра на Raylib");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Привет, Raylib!", screenWidth/2 - MeasureText("Привет, Raylib!", 20)/2, screenHeight/2 - 10, 20, BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
